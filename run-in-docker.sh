e1ccacaf12e076939de85133aa08d46d45cd2b10
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-10-03 23:22:23 +0200

    updated to conform to 3 argument interface

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -2,22 +2,32 @@
 
 # Synopsis:
 # Test runner for run.sh in a docker container
-# Takes the same arguments as run.sh (EXCEPT THAT SOLUTION PATH IS RELATIVE)
+# Takes the same arguments as run.sh (EXCEPT THAT SOLUTION AND OUTPUT PATH ARE RELATIVE)
 # Builds the Dockerfile
 # Runs the docker image passing along the initial arguments
 
 # Arguments:
 # $1: exercise slug
 # $2: **RELATIVE** path to solution folder (without trailing slash)
+# $3: **RELATIVE** path to output directory (without trailing slash)
 
 # Output:
-# [For now] writes the tests output to the terminal
+# Writes the tests output to the output directory
 
 # Example:
-# ./run-in-docker.sh two-fer ./relative/path/to/two-fer/solution/folder
+# ./run-in-docker.sh two-fer ./relative/path/to/two-fer/solution/folder ./relative/path/to/output-directory
+
+# If arguments not provided, print usage and exit
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo "usage: ./run-in-docker.sh two-fer ./relative/path/to/two-fer/solution/folder ./relative/path/to/output-directory"
+fi
 
 # build docker image
 docker build -t javascript-test-runner .
 
 # run image passing the arguments
-docker run -v $PWD/$2:/solution javascript-test-runner $1 /solution
+# run image passing the arguments
+docker run \
+    --mount type=bind,src=$PWD/$2,dst=/solution \
+    --mount type=bind,src=$PWD/$3,dst=/output \
+    javascript-test-runner $1 /solution /output
