10a76333bab90a7ec5b22d443677abab5e72fcd2
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2019-10-03 23:21:41 +0200

    updated to conform to 3 argument interface

--- run.sh
+++ run.sh
@@ -7,18 +7,37 @@
 # Arguments:
 # $1: exercise slug
 # $2: path to solution folder (without trailing slash)
+# $3: path to output directory (without trailing slash)
 
 # Output:
-# [For now] writes the tests output to the terminal
+# Writes the tests output to the output directory
 
 # Example:
-# ./run.sh two-fer path/to/two-fer/solution/folder
+# ./run.sh two-fer path/to/two-fer/solution/folder path/to/output-directory
 
 # Put together the path to the test file
-test_file=$2/$1.spec.js
+test_file="$2/$1.spec.js"
+
+# Put together the path to the test results file
+result_file="$2/results.xml"
 
 # change xtest to test so all tests are run
 sed -i 's/xtest/test/g' $test_file
 
+# install dependencies
 npm i --prefix $2
-npm run test --prefix $2
+npm i --prefix $2 jest-junit
+
+# add custom test run to testfile
+# this is a hack to avoid jest cli complications
+# TODO: find a clean way to use jest cli
+if grep -q '^.*customTest' "$2/package.json"; then
+    echo "hello from replace"
+    sed -i "s#^\s*\"customTest\":.*#\"customTest\": \"jest --no-cache \.\/\* --outputFile=${result_file} --reporters=jest-junit\",#g" "$2/package.json"
+else
+    echo "hello from insert after"
+    sed -i "/^\s*\"test\":.*/a \"customTest\": \"jest --no-cache \./* --outputFile=${result_file} --reporters=jest-junit\"," "$2/package.json"
+fi
+
+# run tests
+npm run customTest --prefix $2
