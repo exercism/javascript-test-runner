6ae2f0bcbf27cb11cd9ec889658f434cb9aae26b
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2019-10-23 14:32:56 +0200

    Cleanup, secure container and pre-install packages

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,26 @@
+FROM node:lts-alpine as builder
+
+# Install SSL ca certificates
+RUN apk update && apk add ca-certificates
+RUN apk upgrade
+
+# Create appuser
+RUN adduser -D -g '' appuser
+
+WORKDIR /javascript-test-runner
+COPY . .
+
+# Only install the node_modules we need
+RUN yarn install --production --modules-folder './production_node_modules'
+
+# Build a minimal and secured container
 FROM node:lts-alpine
-
-RUN apk update && apk upgrade
-
+COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+COPY --from=builder /etc/passwd /etc/passwd
+COPY --from=builder /javascript-test-runner/package.json /opt/test-runner/package.json
+COPY --from=builder /javascript-test-runner/bin /opt/test-runner/bin
+COPY --from=builder /javascript-test-runner/production_node_modules /opt/test-runner/node_modules
+USER appuser
 WORKDIR /opt/test-runner
 
 COPY ./run.sh ./bin/
---
+++ bin/run.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Synopsis:
+# Automatically tests exercism's JS track solutions against corresponding test files.
+# Takes two arguments and makes sure all the tests are run
+
+# Arguments:
+# $1: exercise slug
+# $2: path to solution folder (without trailing slash)
+# $3: path to output directory (without trailing slash)
+
+# Output:
+# Writes the tests output to the output directory
+
+# Example:
+# ./run.sh two-fer path/to/two-fer/solution/folder path/to/output-directory
+
+# Put together the path to the test file
+test_file="$2/$1.spec.js"
+
+# Put together the path to the test results file
+result_file="$2/results.xml"
+package_json_source = "package.json"
+package_json="$2/package.json"
+
+# Change xtest to test so all tests are run
+sed -i 's/xtest/test/g' $test_file
+sed -i 's/xit/it/g' $test_file
+
+# Symlink the node_modules we have, then install missing packages
+ln -sf production_node_modules $2/node_modules
+yarn install --cwd $2
+
+# Afterwards, copy the Package.json
+cp $package_json_source $package_json
+sed -i "s/{result_file}/${result_file}/g" $package_json
+
+yarn test:cli --cwd $2
