8225b51c970dbb6f99de0d78787326edcb426159
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2020-12-24 23:17:52 +0100

    Add CI workflows and scripts

---
+++ bin/pr-check.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Documentation on @
+#   https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#index-_0040
+# Documentation on shift [n]
+#   https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#index-shift
+# Why quoting the argument is "necessary":
+#   https://stackoverflow.com/questions/4824590/propagate-all-arguments-in-a-bash-shell-script/4824637#4824637
+
+# eats 1 argument
+shift 1
+
+lint.sh  "$@"
+check-formatting.sh  "$@"
