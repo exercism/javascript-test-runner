48b5dbd2496df9fc139d23a60a6f5712a08c1bc3
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2021-01-04 18:15:31 +0100

    Update interface and use @exercism/static-analysis
    - Add `test_code` when available
    - Fix `output` when there is none (previously `""`, now `null`)

---
+++ test/fixtures/two-fer/fail/tests/two-fer.js
@@ -0,0 +1,6 @@
+console.log("Some global log")
+
+export const twoFer = (name) => {
+  console.log(name)
+  return `One for you, one for me.`
+}
--- test/fixtures/two-fer/fail/two-fer.js
+++
@@ -1,1 +0,0 @@
-export const twoFer = () => `One for you, one for me.`
