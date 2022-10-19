8cae8fb1a5aec0b3b8b4ba70cb8243afa4068e57
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2022-10-19 22:37:20 +0200

    Cleanup after syncing with typescript-test-runner (#405)

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,6 @@
 // This file is only used by jest.runner.config.js, when running the
 // test-runner. The tool itself uses typescript's compilation instead.
 module.exports = {
-  presets: [
-    [
-      require('@babel/preset-env'),
-      {
-        targets: {
-          node: 'current',
-        },
-        useBuiltIns: 'usage',
-        corejs: '3.25',
-      },
-    ],
-  ],
+  presets: ['@exercism/babel-preset-javascript'],
   plugins: [],
 }
