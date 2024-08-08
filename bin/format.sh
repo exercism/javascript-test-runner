1c7e2b82abd6463ed67d3bc15ccfed3c9a66f205
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2024-08-09 01:06:22 +0200

    Use pnpm list instead of why

--- bin/format.sh
+++ bin/format.sh
@@ -4,7 +4,7 @@ set -uo pipefail
 
 if [ -z "${EXERCISM_PRETTIER_VERSION:-}" ]; then
   echo "[format] pulling prettier version from package.json using sed"
-  EXERCISM_PRETTIER_VERSION="$(corepack pnpm why prettier --parseable | sed -n -e '1,$s/^.*prettier@//' -e 's/\\node_modules\\prettier//p')"
+  EXERCISM_PRETTIER_VERSION="$(corepack pnpm list prettier --parseable | sed -n -e '1,$s/^.*prettier@//' -e 's/\\node_modules\\prettier//p')"
   echo "[format] expected version is now ${EXERCISM_PRETTIER_VERSION:-}"
 fi
 
@@ -17,17 +17,17 @@ if [ -z "${EXERCISM_PRETTIER_VERSION:-}" ]; then
   echo "---------------------------------------------------"
   echo "$(corepack pnpm -v)"
   echo ""
-  echo "This is what corepack pnpm why reports:"
-  echo "$ corepack pnpm why prettier --parseable"
-  echo "$(corepack pnpm why prettier --parseable)"
+  echo "This is what corepack pnpm list reports:"
+  echo "$ corepack pnpm list prettier --parseable"
+  echo "$(corepack pnpm list prettier --parseable)"
   echo ""
   echo "And corepack pnpm info reports the following:"
   echo "$ corepack pnpm info prettier"
   echo "$(corepack pnpm info prettier)"
   echo ""
   echo "This is the version that can be extracted using sed:"
-  echo "$ corepack pnpm why prettier --parseable | sed -n -e '1,\$s/^.*prettier@//' -e 's/\\node_modules\\prettier//p'"
-  echo "└─ $(corepack pnpm why prettier --parseable | sed -n -e '1,$s/^.*prettier@//' -e 's/\\node_modules\\prettier//p')"
+  echo "$ corepack pnpm list prettier --parseable | sed -n -e '1,\$s/^.*prettier@//' -e 's/\\node_modules\\prettier//p'"
+  echo "└─ $(corepack pnpm list prettier --parseable | sed -n -e '1,$s/^.*prettier@//' -e 's/\\node_modules\\prettier//p')"
   echo ""
   echo "These files are found in the repo root:"
   echo "$(ls -p | grep -v /)"
