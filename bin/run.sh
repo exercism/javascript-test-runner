f40a7995102d9f7384460ec645ee0a3522198eda
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2024-08-11 18:28:57 +0200

    Print ls in case of not executable

--- bin/run.sh
+++ bin/run.sh
@@ -330,6 +330,11 @@ else
   echo "💥  jest executable missing at ${bin_jest} or not executable"
   echo "👁️  ${bin_jest} -> $(realpath "${bin_jest}")"
   echo "   ${COREPACK_ROOT_DIR} as corepack root"
+  if [[ -f "${bin_jest}"]]; then
+    echo "It does exist"
+    ls -l "${bin_jest}"
+  fi
+
   exit -1
 fi;
 
