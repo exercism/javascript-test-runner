f200f0070ddee08742749b4ed95e4b5a54b7683f
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2024-10-18 18:55:06 +0200

    Fix expected results for clock

--- test/dev.test.mjs
+++ test/dev.test.mjs
@@ -6,7 +6,7 @@ import { fixtures } from './paths.mjs'
 // corepack pnpm dlx cross-env SILENT=0 corepack pnpm node test/dev.test.mjs
 
 assertPass(
-  'lasagna',
-  join(fixtures, 'lasagna', 'pass'),
-  join(fixtures, 'lasagna', 'pass')
+  'clock',
+  join(fixtures, 'clock', 'pass'),
+  join(fixtures, 'clock', 'pass')
 )
